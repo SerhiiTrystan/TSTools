8456192419bcfa7f9eeca38412dd557867dc25b7
SerhiiTrystan <salingartist@gmail.com>
2025-07-28 20:37:09 +0200

    second commit

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 bl_info = {
-    "name": "My Addon Name",
+    "name": "SerhiiTrystan",
     "description": "Description of this addon",
     "author": "Authors name",
-    "version": (0, 0, 1),
-    "blender": (2, 9, 0),
+    "version": (0, 0, 2),
+    "blender": (4, 5, 0),
     "location": "View3D",
     "warning": "This addon is still in development.",
     "wiki_url": "",
